5c388eca1f4229dd7548fe47f3cd3b0ccea27b62
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-13 17:59:31 +0400

    Update __init__ method and tests for him.

--- src/channel.py
+++ src/channel.py
@@ -12,6 +12,14 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel_dkt = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel_dkt['items'][0]['snippet']['title']
+        self.description = channel_dkt['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.channel_id
+        self.subs_count = channel_dkt['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel_dkt['items'][0]['statistics']['videoCount']
+        self.view_count = channel_dkt['items'][0]['statistics']['viewCount']
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -9,6 +9,11 @@ def get_item():
 
 def test_channel(get_item):
     assert get_item.channel_id == 'UCMCgOm8GZkHp8zJ6l7_hIuA'
+    assert get_item.title == "вДудь"
+    assert get_item.description == "Здесь задают вопросы"
+    assert int(get_item.subs_count) >= 10300000
+    assert int(get_item.video_count) >= 168
+    assert int(get_item.view_count) >= 1996539103
 
 
 def test_print_info(get_item):
