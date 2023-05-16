c8ede30eb19c42c334c8e30b29ef3a4dd23a7945
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-16 15:14:07 +0400

    Added dunder method to class Channel.

--- src/channel.py
+++ src/channel.py
@@ -17,9 +17,34 @@ class Channel:
         self.title = channel_dct['items'][0]['snippet']['title']
         self.description = channel_dct['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subs_count = channel_dct['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel_dct['items'][0]['statistics']['videoCount']
-        self.view_count = channel_dct['items'][0]['statistics']['viewCount']
+        self.subs_count = int(channel_dct['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel_dct['items'][0]['statistics']['videoCount'])
+        self.view_count = int(channel_dct['items'][0]['statistics']['viewCount'])
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.title}", "{self.__channel_id}", {self.subs_count})'
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subs_count) + int(other.subs_count)
+
+    def __sub__(self, other):
+        return int(self.subs_count) - int(other.subs_count)
+
+    def __lt__(self, other):
+        return int(self.subs_count) < int(other.subs_count)
+
+    def __le__(self, other):
+        return int(self.subs_count) <= int(other.subs_count)
+
+    def __gt__(self, other):
+        return int(self.subs_count) > int(other.subs_count)
+
+    def __ge__(self, other):
+        return int(self.subs_count) >= int(other.subs_count)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
